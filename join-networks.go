ae544645ae6059ca1b444813e0fa764e33eea9ac
Brian Palmer <brian@codekitchen.net>
2017-04-10 14:50:49 -0600

    fix for docker 17.04 list networks API change

--- join-networks.go
+++ join-networks.go
@@ -79,8 +79,13 @@ func getActiveBridgeNetworks(client *docker.Client, containerName string) (netwo
 		panic(err)
 	}
 
-	for _, net := range allNetworks {
-		if net.Driver == "bridge" {
+	for _, netOverview := range allNetworks {
+		if netOverview.Driver == "bridge" {
+			// grab the network details (including the list of containers)
+			net, err := client.NetworkInfo(netOverview.ID)
+			if err != nil {
+				panic(err)
+			}
 			_, containsSelf := net.Containers[containerName]
 			if net.Options["com.docker.network.bridge.default_bridge"] == "true" ||
 				len(net.Containers) > 1 ||
